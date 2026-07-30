b4a107e58de649cac5d88a3cd8ed5f5669961507
Shiv PAnkhaniya <shivpankhaniya@gmail.com>
2026-07-30 21:32:26 +0530

    Add javascript folder

---
+++ javascript/app.js
@@ -0,0 +1,63 @@
+console.log("Hello World!");
+
+let a = 30;
+let b = 45;
+sum = a + b;
+
+console.log("sum of two number" + sum);
+
+let meggi = 100;
+let pasta = 50;
+
+let total = "The total price is " + (meggi + pasta) + " " + "Rupees.";
+let Total = (`Total price is ${meggi + pasta} Rupees.`);
+
+console.log(total);
+console.log(total);
+ 
+//perator
+let A = 15;
+let B = 6;
+
+//Arithematic Operator
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(A ** B);
+console.log(A % B);
+console.log(A / B);
+
+//Unary Operator
+console.log(A++);
+console.log(++A);
+console.log(A--);
+console.log(--A);
+
+//Assignment Operator  
+A = B;
+console.log(A);
+A += B;
+console.log(A);
+A -= B;
+console.log(A);
+A *= B;
+console.log(A);
+A /= B;
+console.log(A);
+A **= B;
+console.log(A);
+A %= B;
+console.log(A);
+
+//Comparison Operator
+
+
+console.log(A == B);  // false
+console.log(A < B);  // true
+console.log(A > B);  // false
+console.log(A != B); // true
+console.log(A <= B); // true
+console.log(A >= B); // false
+
+
+
